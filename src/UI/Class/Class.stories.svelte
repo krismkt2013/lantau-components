24656508af2b529093797b19ae1bcd2fc1a2cb54
Kris Ma <krismkt2013@gmail.com>
2025-06-28 16:13:04 +0800

    Update Project Config

---
+++ src/UI/Class/Class.stories.svelte
@@ -0,0 +1,12 @@
+<script module>
+	import Class from '@components/Class/Class.svelte';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	const { Story } = defineMeta({
+		title: 'Class Diagram/Class',
+		component: Class,
+		tags: ['autodocs']
+	});
+</script>
+
+<Story name="Primary" />
---
+++ src/UI/Class/Class.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '@rootStyles';
+</script>
+
+<div>test</div>
+
+<style>
+	div {
+		color: red;
+		font-size: 20px;
+	}
+</style>
