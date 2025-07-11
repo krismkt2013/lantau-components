9f47c912f18b1409df603ab6d121b2f5fd225a70
Ma, Kris KT <kris.kt.ma@pccw.com>
2025-07-11 18:44:50 +0800

    update MagneticPad

--- src/UI/MagneticContainer/magneticPointsStore.svelte.ts
+++
@@ -1,4 +0,0 @@
-import { writable } from 'svelte/store';
-import type { Point } from '@components/MagneticContainer/types';
-
-export const allMagneticPoints = writable<Point[]>([]);
---
+++ src/UI/MagneticPad/MagneticPad.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { setContext } from 'svelte';
+  import { writable } from 'svelte/store';
+  import type { Point } from '@components/MagneticContainer/types';
+
+  const containerPositions = writable<Record<string, Point>>({});
+  setContext('container-positions', containerPositions);
+
+  export const addContainer = (id: string, position: Point) => {
+    containerPositions.update(positions => {
+      positions[id] = position;
+      return positions;
+    });
+  };
+
+  export const updateContainerPosition = (id: string, position: Point) => {
+    containerPositions.update(positions => {
+      positions[id] = position;
+      return positions;
+    });
+  };
+</script>
+
+<div class="magnetic-pad">
+  <slot />
+</div>
