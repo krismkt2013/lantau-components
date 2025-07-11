f180a0e4692265d66e75df49b913466a282a1b88
Ma, Kris KT <kris.kt.ma@pccw.com>
2025-07-11 13:49:06 +0800

    implement new Component MagneticLine

---
+++ src/UI/MagneticLine/MagneticLine.stories.svelte
@@ -0,0 +1,46 @@
+<script module lang="ts">
+	import MagneticLine from './MagneticLine.svelte';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	const { Story } = defineMeta({
+		title: 'UI/MagneticLine',
+		component: MagneticLine,
+		tags: ['autodocs'],
+		argTypes: {
+			startPoint: { control: 'object' },
+			endPoint: { control: 'object' },
+			showStartArrow: { control: 'boolean' },
+			showEndArrow: { control: 'boolean' },
+		},
+	});
+</script>
+
+<Story name="Default" args={{ startPoint: { x: 50, y: 50 }, endPoint: { x: 200, y: 50 }, showStartArrow: false, showEndArrow: false }}>
+	<div style="position: relative; height: 400px;">
+		<MagneticLine startPoint={{ x: 50, y: 50 }} endPoint={{ x: 200, y: 50 }} />
+	</div>
+</Story>
+
+<Story name="WithStartArrow" args={{ startPoint: { x: 50, y: 50 }, endPoint: { x: 200, y: 50 }, showStartArrow: true, showEndArrow: false }}>
+	<div style="position: relative; height: 400px;">
+		<MagneticLine startPoint={{ x: 50, y: 50 }} endPoint={{ x: 200, y: 50 }} showStartArrow />
+	</div>
+</Story>
+
+<Story name="WithEndArrow" args={{ startPoint: { x: 50, y: 50 }, endPoint: { x: 200, y: 50 }, showStartArrow: false, showEndArrow: true }}>
+	<div style="position: relative; height: 400px;">
+		<MagneticLine startPoint={{ x: 50, y: 50 }} endPoint={{ x: 200, y: 50 }} showEndArrow />
+	</div>
+</Story>
+
+<Story name="WithBothArrows" args={{ startPoint: { x: 50, y: 50 }, endPoint: { x: 200, y: 50 }, showStartArrow: true, showEndArrow: true }}>
+	<div style="position: relative; height: 400px;">
+		<MagneticLine startPoint={{ x: 50, y: 50 }} endPoint={{ x: 200, y: 50 }} showStartArrow showEndArrow />
+	</div>
+</Story>
+
+<Story name="AngledLine" args={{ startPoint: { x: 50, y: 50 }, endPoint: { x: 200, y: 50 }, showStartArrow: true, showEndArrow: true }}>
+	<div style="position: relative; height: 400px;">
+		<MagneticLine startPoint={{ x: 50, y: 50 }} endPoint={{ x: 200, y: 50 }} showStartArrow showEndArrow />
+	</div>
+</Story>
---
+++ src/UI/MagneticLine/MagneticLine.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import './MagneticLine.css';
+	export let startPoint: { x: number; y: number };
+	export let endPoint: { x: number; y: number };
+	export let showStartArrow: boolean = false;
+	export let showEndArrow: boolean = false;
+
+	$: length = Math.sqrt(Math.pow(endPoint.x - startPoint.x, 2) + Math.pow(endPoint.y - startPoint.y, 2));
+	$: angle = Math.atan2(endPoint.y - startPoint.y, endPoint.x - startPoint.x) * 180 / Math.PI;
+</script>
+
+<div class="magnetic-line-container" style="left: {startPoint.x}px; top: {startPoint.y}px; width: {length}px; transform: rotate({angle}deg);">
+	<div class="line"></div>
+	{#if showStartArrow}
+		<div class="arrow-start"></div>
+	{/if}
+	{#if showEndArrow}
+		<div class="arrow-end"></div>
+	{/if}
+</div>
